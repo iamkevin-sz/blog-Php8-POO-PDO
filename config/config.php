b385f41d424a39e8f249afb41bc2682971cfe5e1
kevin_sz <iamkevinnumber1@gmail.com>
2022-07-09 15:30:35 -0400

    blog

---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php
+
+    //Rutas del proyecto
+    //Para el admin
+    define('RUTA_ADMIN', 'http://localhost/blog/admin/');
+
+    //Para el front
+    define('RUTA_FRONT', 'http://localhost/blog/');
